1d6975b711655c70796be43b3b580b8ab9aedd79
Thaddaeus Tay <thtay@c1r3s2.42singapore.sg>
2026-08-24 23:05:55 +0800

    missed this...

--- include/minishell.h
+++ include/minishell.h
@@ -41,16 +41,16 @@ typedef enum e_token_type
 
 typedef struct s_token
 {
-	t_token_type type;
-	char *value;
-	struct s_token  *next;
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
 }	t_token;
 
 typedef struct s_cmd
 {
-	char **av;
-	char *redirs;
-	struct s_cmd *next;
+	char			**av;
+	char			*redirs;
+	struct s_cmd	*next;
 }	t_cmd;
 
 typedef enum e_builtin_cmd
